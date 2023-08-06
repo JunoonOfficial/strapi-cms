4c5d96f17fcce75ce56db520e1bdcbaaca628e8a
Arpit Gupta <arpitgupta1214@gmail.com>
2023-08-06 23:52:43 +0530

    all content types added

---
+++ src/api/collection-group/routes/collection-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * collection-group router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::collection-group.collection-group');
---
+++ src/api/collection-group/services/collection-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * collection-group service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::collection-group.collection-group');
